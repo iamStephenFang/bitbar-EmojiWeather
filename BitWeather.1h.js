4cac21229c0ff0a64c951e77d97b14ac90d918f2
iamStephenFang <Fangxuanmiao@hotmail.com>
2020-03-03 22:22:17 +0800

    修复了天气逻辑的问题
    - 修复了天气逻辑的问题
    - 补全所有天气代码

--- BitWeather.1h.js
+++ BitWeather.1h.js
@@ -1,4 +1,4 @@
-#!/usr/local/bin/node
+#!/usr/bin/env /usr/local/bin/node
 
 /* jshint esversion: 6 */
 
@@ -13,15 +13,11 @@ request(url, function(err, response, body) { // Collect JSON object
     if (err) { // If no connection, so sad...
         console.log(emojis[0]);
     } else {
-        var cond;
         let weather = JSON.parse(body);
-        // console.log(weather.HeWeather6[0].now.cond_code);
-        switch (weather.HeWeather6[0].now.cond_code) {
+        var cond;
+        switch (parseInt(weather.HeWeather6[0].now.cond_code)) {
             case 100:
-                cond = ":sunny:";
-                break;
-            case 101:
-                cond = ":cloud:";
+                cond = "☀️";
                 break;
             case 102:
                 cond = "🌥";
@@ -29,18 +25,39 @@ request(url, function(err, response, body) { // Collect JSON object
             case 103:
                 cond = "⛅️";
                 break;
-            case 104:
-                cond = "Thurscond";
+            case 101, 104:
+                cond = "☁️";
                 break;
-            case 5:
-                cond = "Fricond";
+            case 200, 201, 203, 204:
+                cond = "🎐";
                 break;
-            case 6:
-                cond = "❔";
+            case 205, 206, 207:
+                cond = "💨";
+                break;
+            case 208, 209, 210, 211, 212, 213:
+                cond = "🌪";
+                break;
+            case 300, 301, 302, 303, 310, 311, 312, 315, 316, 317, 318:
+                cond = "⛈";
+                break;
+            case 300, 304, 305, 306, 307, 309, 313, 314, 399:
+                cond = "🌧";
+                break;
+            case 400, 404, 405, 408:
+                cond = "❄️";
+                break;
+            case 401, 402, 403, 406, 407, 409, 410, 499:
+                cond = "❄️";
+                break;
+            case 500, 501, 502, 503, 509, 510, 511, 512, 513, 514, 515:
+                cond = "🌫";
                 break;
             default:
                 cond = "❔";
         }
         console.log(cond);
+        console.log("---");
+        console.log("今天天气为" + weather.HeWeather6[0].now.cond_txt);
     }
+
 });
